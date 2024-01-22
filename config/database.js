0f77ffdaebf056376f95c522750fe14912d4abbc
Ankit Tanwar <tankitofficial01@gmail.com>
2024-01-22 14:39:47 +0530

    new

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),  // Use the domain only
+        host: env('DATABASE_HOST', 'aws-0-ap-south-1.pooler.supabase.com'),  // Use the domain only
         port: env.int('DATABASE_PORT', 6543),
         database: env('DATABASE_NAME', 'postgres'),
         username: env('DATABASE_USERNAME', 'postgres.jsggjeqxlxabceoqnetg'),
-        password: env('DATABASE_PASSWORD', 'mypassword'),
+        password: env('DATABASE_PASSWORD', '@Ankithubc01'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {},
