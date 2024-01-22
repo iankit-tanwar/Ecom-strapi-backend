a2c62b088c7eccfa951099348cdb4d776dc92c82
Ankit Tanwar <tankitofficial01@gmail.com>
2024-01-22 14:25:36 +0530

    new

--- config/database.js
+++ config/database.js
@@ -5,12 +5,8 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'aws-0-ap-south-1.pooler.supabase.com'),  // Use the domain only
-        port: env.int('DATABASE_PORT', 5432),  // Update to the correct port
-        database: env('DATABASE_NAME', 'postgres'),
-        username: env('DATABASE_USERNAME', 'postgres.jsggjeqxlxabceoqnetg'),
-        password: env('DATABASE_PASSWORD', 'mypassword'),
-        ssl: true,  // Set to true if your PostgreSQL database requires SSL
+        url: env('DATABASE_URL', 'postgres://user:password@localhost:5432/database'), // Replace with your actual URL
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {},
     },
